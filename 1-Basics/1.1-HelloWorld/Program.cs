82d1c055a999cf6e68c4c0547b906868327fe3a7
vvhatislovee <dyundyukovv@gmail.com>
2025-12-19 12:32:29 +0600

    Create Program.cs

---
+++ 1-Basics/1.1-HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Task1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
